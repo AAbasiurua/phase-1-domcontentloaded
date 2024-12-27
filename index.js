f0e69d26d540e385bc1ca71d6110848bde8f371f
AniekanabasiAbasiurua <aniekanabasi.abasiurua@gmail.com>
2024-12-26 20:20:56 -0500

    phase-1-domcontentloaded-complete

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded"); 
+});
+
+    "This console.log() fires when index.js loads - before DOMContentLoaded has triggered"
+;// Your code goes here
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('text').innerHTML = `This is really cool!`;
+})
